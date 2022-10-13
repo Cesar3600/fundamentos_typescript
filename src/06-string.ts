8030d647af219eca17df9cf4bfd35b88f77422bd
Cesar3600 <cesar.contreras.medina.3600@gmail.com>
2022-10-12 19:50:35 -0500

    06 string

---
+++ src/06-string.ts
@@ -0,0 +1,23 @@
+(() => {
+  let productTitle = 'My amazing product';
+
+  productTitle = 'My amazing product changed';
+  console.log('productTitle', productTitle);
+
+  const productDescription = 'Im bla bla bla bla  s asasas';
+  console.log('productDescription', productDescription);
+
+  let productPrice = 100;
+  let isNew: boolean = false;
+
+  const summary = `
+    title:${productTitle}
+    description:${productDescription}
+    price:${productPrice}
+    isNew:${isNew}
+  `;
+
+  console.log(summary);
+
+  const myString: string = '';
+})();
