2c1a13cc093ef1ad268a657a296f3a2ed4901ad0
Cesar3600 <cesar.contreras.medina.3600@gmail.com>
2022-10-13 01:23:06 -0500

    13 retorno de funciones

---
+++ src/13-return-funcions.ts
@@ -0,0 +1,16 @@
+() => {
+  const calcTotal = (prices: number[]): string => {
+    let total = 0;
+    prices.forEach((item) => {
+      total += item;
+    });
+    return total.toString();
+  };
+
+  const printTotal = (prices: number[]): void => {
+    const rpta = calcTotal(prices);
+    console.log(`El total es ${rpta}`);
+  };
+
+  printTotal([1, 2, 3, 4, 5, 6, 7]);
+};
